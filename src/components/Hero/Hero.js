7e2f3f36b1e35504ca5a115c60edf88b0867daa2
Owen Carruth <ocarr199@gmail.com>
2021-07-18 16:29:03 -0500

    spelling

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,11 +8,11 @@ const Hero = () => (
  <Section row nopadding>
    <LeftSection>
      <SectionTitle main center>
-     Hello, I Am<br/>
+     Hello, I'm<br/>
       Owen Carruth
      </SectionTitle>
      <SectionText>
-       I am a software developer who is suer excited about all of the amazing things I can do with technology.
+       I am a software developer who is super excited about all of the amazing things I can do with technology.
      </SectionText>
      {/* <Button onClick={() => window.location = ""}>Learn More</Button> */}
    </LeftSection>
